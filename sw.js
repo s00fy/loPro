7875484fd2c2a0431f906e3ad7debdec65a8048d
Sophie <sophie.garreta@gmail.com>
2023-12-13 23:50:02 +0100

    pwa

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Choose a cache name
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v1";
+
+// List the files to precache
+const assets = [
+  "/index.html",
+  "/src/css/main.css",
+  "/src/css/reset.css",
+  "/src/js/content.js",
+  "/src/js/displayPadding.js",
+  "/src/js/exemple.js",
+  "/src/fonts/Outfit-VariableFont_wght.ttf",
+  "/src/fonts/YounxgSerif-Regular.ttf",
+  "/src/icons/icon-48x48.png",
+  "/src/icons/icon-72x72.png",
+  "/src/icons/icon-96x96.png",
+  "/src/icons/icon-128x128.png",
+  "/src/icons/icon-144x144.png",
+  "/src/icons/icon-152x152.png",
+  "/src/icons/icon-192x192.png",
+  "/src/icons/icon-384x384.png",
+  "/src/icons/icon-512x512.png",
+];
